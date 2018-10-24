88153bf0e8178532dc82a98a5ed1c5398a16c743
Mohammad Arkan Mufadho <32085809+mufendew@users.noreply.github.com>
2018-10-24 20:30:10 +0700

    Update application/controllers/Propil/Googlee.php

--- application/controllers/Propil/Googlee.php
+++ application/controllers/Propil/Googlee.php
@@ -98,9 +98,9 @@ class Googlee extends CI_Controller{
 					$this->session->set_userdata('DataProfile',$dataNIM);
 					redirect('dashboard');
 				}
-				$data['userdata'] = $this->session->userdata('DataGoogle');
-				$this->load->view('Profile/V_After',$data);
 			}
+			$data['userdata'] = $this->session->userdata('DataGoogle');
+			$this->load->view('Profile/V_After',$data);
 		}else redirect('','refresh');
 	}
 	
